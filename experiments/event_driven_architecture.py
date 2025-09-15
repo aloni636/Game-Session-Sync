121f08607afa4fb907e521370e86f1e976a8f180
aloni636 <elyasaf.aloni@gmail.com>
2025-09-15 11:58:50 +0300

    feat(experiments): implement most basic event-driven architecture with producer-consumer model

---
+++ experiments/event_driven_architecture.py
@@ -0,0 +1,37 @@
+import asyncio
+from asyncio import Queue
+
+
+class ConsoleProducer:
+    def __init__(self, queue: Queue) -> None:
+        self.queue = queue
+
+    async def run(self) -> None:
+        while True:
+            line = await asyncio.to_thread(input, "> ")
+            await self.queue.put(line)
+            print(f"[producer] put item: {line}")
+
+
+class Consumer:
+    def __init__(self, queue: Queue) -> None:
+        self.queue = queue
+
+    async def run(self) -> None:
+        while True:
+            item = await self.queue.get()
+            print(f"[consumer] consumed item: {item}")
+            self.queue.task_done()
+            print(f"[consumer] remaining items: {self.queue.qsize()}")
+
+
+async def main():
+    queue = Queue()
+    await asyncio.gather(ConsoleProducer(queue).run(), Consumer(queue).run())
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("\nexiting.")
