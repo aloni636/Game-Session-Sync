94f4c8d9d1a7b4db6355340c7c87d16b6a9fa9f6
aloni636 <elyasaf.aloni@gmail.com>
2025-10-29 04:42:13 +0200

    fix(log): copied Console to log_helpers to avoid experiments not being found in prod-venv after poetry build

--- game_session_sync/log_helpers.py
+++ game_session_sync/log_helpers.py
@@ -1,11 +1,41 @@
 import dataclasses
 import logging
+import subprocess
 from logging.handlers import RotatingFileHandler
 from pprint import pformat
+from typing import IO, cast
 
-# TODO: Isolate experiments by reimplementing Console here
-from experiments.console import Console
 from game_session_sync.constants import LOG_PATH
+
+
+# Open a new console window running PowerShell that echoes stdin lines
+class Console:
+    def __init__(self) -> None:
+        self._p = subprocess.Popen(
+            [
+                "powershell.exe",
+                "-NoLogo",
+                "-NoExit",
+                "-Command",
+                # Read from $input and print each line as-is
+                "$input | ForEach-Object { $_ }",
+            ],
+            creationflags=subprocess.CREATE_NEW_CONSOLE,
+            stdin=subprocess.PIPE,
+            text=True,  # send str, not bytes
+            bufsize=1,  # line-buffered
+        )
+        self.stdin = self._p.stdin
+
+    def print(self, line: str):
+        self._p.stdin = cast(
+            IO[str], self._p.stdin
+        )  # tells pylint p.stdin cannot be None
+        self._p.stdin.write(line + "\n")
+        self._p.stdin.flush()
+
+    def kill(self):
+        self._p.kill()
 
 
 def dataclass_format(dataclass) -> str:
